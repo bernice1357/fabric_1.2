30742f6e13bd680c5f77027a2249c0fd93ed8092
bernice1357 <juice6323@gmail.com>
2022-05-11 15:56:12 +0800

    feat: BugFix

--- project1/src/router/index.js
+++ project1/src/router/index.js
@@ -2,10 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 
 import PlaceOrder from '../views/PlaceOrder.vue'
-import Orders from '../views/Orders.vue'
-import Deal from '../views/Deal.vue'
 import AllOrders from '../views/AllOrders.vue'
-import Add from '../views/Add.vue'
 import Home from '../views/Home.vue'
 import Login from '../views/Login.vue'
 import Register from '../views/Register.vue'
@@ -13,21 +10,9 @@ import Register from '../views/Register.vue'
 Vue.use(VueRouter)
 const routes = [
     {
-        path: '/orders',
-        name: 'Orders',
-        component: Orders
-    },{
         path: '/placeOrder',
         name: 'PlaceOrder',
         component: PlaceOrder
-    },{
-        path: '/deal',
-        name: 'Deal',
-        component: Deal
-    },{
-      path: '/add',
-      name: 'Add',
-      component: Add
     },{
         path: '/',
         name: 'Home',
